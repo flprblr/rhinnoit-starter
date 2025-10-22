f491b13b485405dbdd9f7250109c52b8bd45b9e0
Felipe Rubilar <frubilar@rhinnoit.cloud>
2025-10-22 20:41:28 -0300

    Permissions enabled

---
+++ app/Models/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Role extends \Spatie\Permission\Models\Role
+{
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+}
