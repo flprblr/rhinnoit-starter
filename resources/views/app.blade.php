81ad2327da21cbebc0767280b530d994eb3bee83
Felipe Rubilar <frubilar@rhinnoit.cloud>
2025-10-28 15:40:24 -0300

    Native fonts enabled

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,9 +36,6 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
